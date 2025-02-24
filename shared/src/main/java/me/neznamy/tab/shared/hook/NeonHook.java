319d7d7d54d31300ee4666789e461d971219a8f4
Decameter <45654930+Decameter@users.noreply.github.com>
2025-02-24 22:22:38 +0200

    fix: fixed an error with fetching disguises

--- shared/src/main/java/me/neznamy/tab/shared/hook/NeonHook.java
+++ shared/src/main/java/me/neznamy/tab/shared/hook/NeonHook.java
@@ -3,14 +3,36 @@ package me.neznamy.tab.shared.hook;
 import lombok.SneakyThrows;
 import me.neznamy.tab.shared.platform.TabPlayer;
 
+import java.lang.reflect.Method;
 import java.util.UUID;
 
 public class NeonHook {
 
-    @SneakyThrows
-    public static Object getDisguise(TabPlayer p) {
-        final Object instance = Class.forName("net.leonemc.neon.spigot.Neon").getDeclaredMethod("getInstance").invoke(null);
-        return instance.getClass().getMethod("getDisguiseManager").invoke(instance).getClass().getMethod("get", UUID.class).invoke(instance, p.getUniqueId());
+    private static Method getInstance;
+    private static Method getDisguiseManager;
+    private static Method getDisguise;
+
+    static {
+        try {
+            Class<?> neonClass = Class.forName("net.leonemc.neon.spigot.Neon");
+            getInstance = neonClass.getDeclaredMethod("getInstance");
+
+            Class<?> disguiseManagerClass = Class.forName("net.leonemc.neon.spigot.features.disguise.DisguiseManager");
+            getDisguiseManager = neonClass.getMethod("getDisguiseManager");
+            getDisguise = disguiseManagerClass.getMethod("get", UUID.class);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 
+    @SneakyThrows
+    public static Object getDisguise(TabPlayer p) {
+        if (getInstance == null || getDisguiseManager == null || getDisguise == null) {
+            return null;
+        }
+
+        Object neonInstance = getInstance.invoke(null);
+        Object disguiseManager = getDisguiseManager.invoke(neonInstance);
+        return getDisguise.invoke(disguiseManager, p.getUniqueId());
+    }
 }
